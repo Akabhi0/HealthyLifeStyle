b507455e94f5863ba9d96e53f40c0b62bb0e1645
abhishek abhi <abhishek@glintlogics.com>
2019-06-26 14:56:37 +0530

    26/5/2019 heart dieases risk calculator

---
+++ app/src/main/java/com/dev/healthylifestyle/ui/patient/viewModel/BMICalculatorViewModel.java
@@ -0,0 +1,39 @@
+package com.dev.healthylifestyle.ui.patient.viewModel;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.dev.healthylifestyle.ui.patient.model.BMIArrayModel;
+import com.dev.healthylifestyle.ui.patient.model.BMISendModel;
+import com.dev.healthylifestyle.ui.patient.model.CommonModel;
+import com.dev.healthylifestyle.ui.patient.repository.BMIRepository;
+
+public class BMICalculatorViewModel extends ViewModel {
+
+    private BMIRepository repository;
+    private MutableLiveData<Boolean> onClicked = new MutableLiveData<>();
+
+    public BMICalculatorViewModel() {
+        repository = BMIRepository.getBmiRepository();
+    }
+
+    public MutableLiveData<Boolean> getOnClicked() {
+        return onClicked;
+    }
+
+    public void setOnClicked(Boolean onClicked) {
+        this.onClicked.setValue(onClicked);
+    }
+
+    public MutableLiveData<BMIArrayModel> getBMIArrayModel(int userID) {
+        return repository.getBMIArrayModel(userID);
+    }
+
+    public MutableLiveData<CommonModel> setBMIArrayModel(BMISendModel bmiSendModel) {
+        return repository.sendBMIModel(bmiSendModel);
+    }
+
+    public void onClickSendUpdates() {
+        setOnClicked(true);
+    }
+}
